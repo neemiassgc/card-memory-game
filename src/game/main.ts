bcbbcec97d03101aeba6787e094fc81dd567a59e
Neemias Santos <neemiassgc@outlook.com>
2025-06-08 19:12:15 -0300

    feat(scale): Add scale configuration for responsive canvas size

--- src/game/main.ts
+++ src/game/main.ts
@@ -4,18 +4,24 @@ import { Menu } from './scenes/Menu';
 import { GameEnd } from './scenes/GameEnd';
 import { colors } from '../tools';
 import { Boot } from './scenes/Boot';
+import { Room } from './scenes/Room';
 
-// Find out more information about the Game Config at:
-// https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Types.Core.GameConfig = {
   type: AUTO,
   width: 1080,
   height: 720,
   parent: 'game-container',
   backgroundColor: colors["first"].hex as string,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 1080,
+    height: 720,
+  },
   scene: [
     Boot,
     Menu,
+    Room,
     Gameplay,
     GameEnd,
   ]
